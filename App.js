cbc6faba75abf6f33b02f16860180baf6d32914c
alexukm <a172569@siswa.ukm.edu.my>
2023-05-30 21:35:09 +0800

    feat: init project

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ import UserSignUp from "./src/screens/UserRegisterForm";
 import DriverSignUp from "./src/screens/DriverRegisterForm";
 import DriverRegisterImage from "./src/screens/DriverRegisterImage";
 const Stack = createStackNavigator();
-const test = 1
 export default function App() {
   return (
       <NativeBaseProvider>
